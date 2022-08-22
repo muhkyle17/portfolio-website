d2b23e651ce175974ab42f1d82769f680a7b797a
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-08-21 18:47:59 -0700

    Make social header component

---
+++ components/Social.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+// import social icons
+import { social } from '../data'
+
+const Social = () => {
+  return (
+    <ul className='flex space-x-6'>
+      {social.map((item, index) => {
+        return (
+          <li
+            className='flex justify-center items-center text-[#FFA112]'
+            key={index}
+          >
+            <a
+              href={item.href}
+              target='_blank'
+              rel='noreferrer'
+              className='text-base'
+            >
+              {item.icon}
+            </a>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default Social
--- components/common/Header.js
+++ components/common/Header.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 // Import components
 import Nav from '../Nav'
+import Social from '../Social'
 
 const Header = () => {
   return (
-    <div className='mt-10 flex justify-between items-center'>
+    <div className='mt-8 flex justify-between items-center'>
       <a href='#' className='text-2xl'>
         Mikyle Reyes
       </a>
       <Nav />
-      <div>Social</div>
+      <Social />
     </div>
   )
 }
