e187cfefedd605e7ef976f90cbddac5d22d057c6
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-08-24 10:19:03 -0700

    Add styling for header when scrolling down and fix other styling

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 // Import components
 import Nav from '../Nav'
@@ -6,23 +6,36 @@ import Social from '../Social'
 import NavMobile from '../NavMobile'
 
 const Header = () => {
-  return (
-    <div className='mt-8 flex justify-between items-center'>
-      {/* logo */}
-      <a href='#' className='text-5xl name'>
-        Mikyle Reyes
-      </a>
+  const [bg, setBg] = useState(false)
 
-      <div className='hidden lg:block'>
-        <Nav />
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      return window.scrollY > 50 ? setBg(true) : setBg(false)
+    })
+  })
+
+  return (
+    <header
+      className={`${
+        bg ? 'bg-tertiary h-20' : 'h-24'
+      } flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300`}
+    >
+      <div className='container mx-auto h-full flex justify-between items-center px-10 lg:px-2'>
+        <a href='#' className='text-5xl name'>
+          Mikyle Reyes
+        </a>
+
+        <div className='hidden lg:block'>
+          <Nav />
+        </div>
+        <div className='hidden lg:block'>
+          <Social />
+        </div>
+        <div className='lg:hidden'>
+          <NavMobile />
+        </div>
       </div>
-      <div className='hidden lg:block'>
-        <Social />
-      </div>
-      <div className='lg:hidden'>
-        <NavMobile />
-      </div>
-    </div>
+    </header>
   )
 }
 
