1f8ece4e29283694e156a419e16be60fa659b8cc
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-08-23 22:51:07 -0700

    Changes to NavMobile component

--- components/common/Header.js
+++ components/common/Header.js
@@ -3,15 +3,25 @@ import React from 'react'
 // Import components
 import Nav from '../Nav'
 import Social from '../Social'
+import NavMobile from '../NavMobile'
 
 const Header = () => {
   return (
     <div className='mt-8 flex justify-between items-center'>
+      {/* logo */}
       <a href='#' className='text-5xl name'>
         Mikyle Reyes
       </a>
-      <Nav />
-      <Social />
+
+      <div className='hidden lg:block'>
+        <Nav />
+      </div>
+      <div className='hidden lg:block'>
+        <Social />
+      </div>
+      <div className='lg:hidden'>
+        <NavMobile />
+      </div>
     </div>
   )
 }
