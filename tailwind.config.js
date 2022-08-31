ba3cdaa436504b55e0d050a0f79c6304eb7eb43c
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-08-30 18:58:32 -0700

    Add colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FFA112',
+        primaryDarker: '#FFA012CB',
+        secondary: '#1C1D24',
+        tertiary: '#131419',
+        accent: {
+          DEFAULT: '#ac6b34',
+          hover: '#925a2b',
+        },
+        paragraph: '#878e99',
+      },
+    },
   },
   plugins: [],
 }
