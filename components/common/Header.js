ba1348828a831e1f766def99a3bb5784b2d5556d
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-08-30 18:49:43 -0700

    Fix color not showing on scroll:

--- components/common/Header.js
+++ components/common/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
   return (
     <header
       className={`${
-        bg ? 'bg-tertiary h-20' : 'h-24'
+        bg ? 'bg-[#131419] h-20' : 'h-24'
       } flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300`}
     >
       <div className='container mx-auto h-full flex justify-between items-center px-10 lg:px-7'>
