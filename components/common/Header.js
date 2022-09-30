f0dcb9ffdb1ea2fca8c20918f62601b9efcd8bae
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-09-29 23:37:47 -0700

    Add transition on hover

--- components/common/Header.js
+++ components/common/Header.js
@@ -21,7 +21,10 @@ const Header = () => {
       } flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300`}
     >
       <div className='container mx-auto h-full flex justify-between items-center px-10 lg:px-7'>
-        <a href='#' className='text-5xl name hover:text-primary'>
+        <a
+          href='#'
+          className='text-5xl name hover:text-primary transition-all duration-300'
+        >
           Mikyle Reyes
         </a>
 
