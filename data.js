6fe861aee8277a67b69cf557c7b058d77028bdab
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-09-30 18:23:26 -0700

    Add skills data and images

--- data.js
+++ data.js
@@ -13,6 +13,17 @@ import {
   FiMapPin,
 } from 'react-icons/fi'
 
+// skills images
+import HtmlSkill from './public/skills/html5.png'
+import CssSkill from './public/skills/css3.png'
+import JsSkill from './public/skills/js.png'
+import Reactjs from './public/skills/reactjs.png'
+import Nextjs from './public/skills/nextjs.png'
+import GitSkill from './public/skills/git.png'
+import RailsSkill from './public/skills/rails.png'
+import TailwindSkill from './public/skills/tailwindcss.png'
+import ApolloSkill from './public/skills/apolloclient.png'
+
 export const navigation = [
   {
     name: 'home',
@@ -49,5 +60,36 @@ export const social = [
   {
     icon: <FiGithub />,
     href: 'https://github.com/muhkyle17',
+  },
+]
+
+// skills
+export const skills = [
+  {
+    image: HtmlSkill,
+  },
+  {
+    image: CssSkill,
+  },
+  {
+    image: JsSkill,
+  },
+  {
+    image: Reactjs,
+  },
+  {
+    image: Nextjs,
+  },
+  {
+    image: GitSkill,
+  },
+  {
+    image: RailsSkill,
+  },
+  {
+    image: TailwindSkill,
+  },
+  {
+    image: ApolloSkill,
   },
 ]
