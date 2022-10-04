a80cf7179e6e0e6a9a7d8c98ca62589ad65e564b
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-10-03 22:38:03 -0700

    Skeleton of the project cards

--- components/Project.js
+++ components/Project.js
@@ -1,13 +1,31 @@
 import React from 'react'
 
-const Project = () => {
+import Image from 'next/image'
+
+import imagePlaceholder from '../public/brewlab.png'
+
+const Project = ({
+  name,
+  picture,
+  skills,
+  description,
+  live,
+  source,
+  bgColor,
+}) => {
   return (
-    <div>
+    <div
+      className={`flex flex-col items-center justify-center gap-3 w-[18rem] ${bgColor}//`}
+    >
       <p>Name</p>
-      <p>Image</p>
+      <Image src={imagePlaceholder} alt='Brew Lab Project' />
       <p>Skills</p>
-      <p>Description</p>
-      <div>
+      <p className='text-center'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+        nesciunt libero ipsa maiores nemo dolor, aliquid ducimus distinctio
+        reprehenderit quibusdam!
+      </p>
+      <div className='flex'>
         <p>Live</p>
         <p>Source</p>
       </div>
--- components/Projects.js
+++ components/Projects.js
@@ -3,8 +3,12 @@ import Project from './Project'
 
 const Projects = () => {
   return (
-    <div className='container mx-auto px-28 flex'>
-      <Project />
+    <div className='container mx-auto px-28 gap-5 flex flex-wrap justify-center items-center bg-teal-600//'>
+      <Project bgColor={'bg-red-400'} />
+      <Project bgColor={'bg-green-400'} />
+      <Project bgColor={'bg-blue-400'} />
+      <Project bgColor={'bg-orange-400'} />
+      <Project bgColor={'bg-teal-400'} />
     </div>
   )
 }
