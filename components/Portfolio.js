be4ca2c2aa285cde17032d0e2fa55e3ece9320cc
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-10-04 23:31:22 -0700

    Fix spacing of portfolio section

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -7,8 +7,8 @@ import Projects from './Projects'
 const Portfolio = () => {
   return (
     <section id='portfolio' className='min-h-[1400px]'>
-      <div className='container mx-auto h-full py-12 lg:py-32'>
-        <div className='flex flex-col gap-5 items-center mb-14'>
+      <div className='container mx-auto h-full py-12 lg:py-16'>
+        <div className='flex flex-col gap-4 items-center mb-6'>
           <h1 className='text-5xl font-bold'>PORTFOLIO</h1>
           <p className='text-2xl'>My latest work</p>
         </div>
