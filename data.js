034614663ee439f55bd9d68826a7c6fa8e418bd9
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-10-04 22:06:06 -0700

    Add project data and pictures to project

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-//  icons
+// Icons
 import {
   // FiYoutube,
   // FiInstagram,
@@ -13,7 +13,7 @@ import {
   FiMapPin,
 } from 'react-icons/fi'
 
-// skills images
+// Skills images
 import HtmlSkill from './public/skills/html5.png'
 import CssSkill from './public/skills/css3.png'
 import JsSkill from './public/skills/js.png'
@@ -23,6 +23,14 @@ import GitSkill from './public/skills/git.png'
 import RailsSkill from './public/skills/rails.png'
 import TailwindSkill from './public/skills/tailwindcss.png'
 // import ApolloSkill from './public/skills/apolloclient.png'
+
+// Projects images
+import BrewLab from './public/projects/brewlab.png'
+import Natours from './public/projects/natours-project.png'
+import Omnifood from './public/projects/omnifood-project.png'
+import Instox from './public/projects/instox.png'
+import TicTacToe from './public/projects/tic-tac-toe.png'
+import WebDevPortfolio from './public/projects/web-dev-portfolio.png'
 
 export const navigation = [
   {
@@ -96,5 +104,54 @@ export const skills = [
   {
     image: TailwindSkill,
     alt: 'TailwindCSS Skill',
+  },
+]
+
+// projects
+export const projects = [
+  {
+    title: 'Brew Lab PH',
+    image: BrewLab,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nesciunt libero ipsa maiores nemo dolor, aliquid ducimus distinctio reprehenderit quibusdam alksjdf alsfei sjfioe jaejfuiw isuf uehfouuosefjw owefjiosej fsj oejfs iosfeooisjf! ',
+    skills: ['HTML', 'CSS', 'Shopify'],
+    live: 'https://brewlab.ph/',
+    source: 'https://github.com/muhkyle17/brewlabph',
+  },
+  {
+    title: 'Instox',
+    image: Instox,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nesciunt libero ipsa maiores nemo dolor, aliquid ducimus distinctio reprehenderit quibusdam alksjdf alsfei sjfioe jaejfuiw isuf uehfouuosefjw owefjiosej fsj oejfs iosfeooisjf! ',
+    skills: ['HTML', 'Ruby on Rails', 'Tailwind CSS'],
+    live: 'https://instox.herokuapp.com/',
+    source: 'https://github.com/bananakiu/stock-trading-app',
+  },
+  {
+    title: 'Natours',
+    image: Natours,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nesciunt libero ipsa maiores nemo dolor, aliquid ducimus distinctio reprehenderit quibusdam alksjdf alsfei sjfioe jaejfuiw isuf uehfouuosefjw owefjiosej fsj oejfs iosfeooisjf! ',
+    skills: ['HTML', 'CSS', 'SCSS'],
+    live: 'https://muhkyle17.github.io/natours-project/',
+    source: 'https://github.com/muhkyle17/natours-project',
+  },
+  {
+    title: 'Tic Tac Toe',
+    image: TicTacToe,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nesciunt libero ipsa maiores nemo dolor, aliquid ducimus distinctio reprehenderit quibusdam alksjdf alsfei sjfioe jaejfuiw isuf uehfouuosefjw owefjiosej fsj oejfs iosfeooisjf! ',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    live: 'https://muhkyle17.github.io/TicTacToe/',
+    source: 'https://github.com/muhkyle17/TicTacToe',
+  },
+  {
+    title: 'Web Dev Portfolio',
+    image: WebDevPortfolio,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nesciunt libero ipsa maiores nemo dolor, aliquid ducimus distinctio reprehenderit quibusdam alksjdf alsfei sjfioe jaejfuiw isuf uehfouuosefjw owefjiosej fsj oejfs iosfeooisjf! ',
+    skills: ['HTML', 'CSS'],
+    live: 'https://muhkyle17.github.io/web-developer-portfolio/',
+    source: 'https://github.com/muhkyle17/web-developer-portfolio',
   },
 ]
