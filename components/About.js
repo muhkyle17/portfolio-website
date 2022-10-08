800d543e019d5ed41862d03dd54c2c1635a1a5ab
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-10-08 16:45:17 -0700

    Add responsiveness to modal for all screen sizes

--- components/About.js
+++ components/About.js
@@ -10,28 +10,40 @@ function Modal() {
   return (
     <div className='h-screen fixed bottom-0 left-0 z-50'>
       <div className='h-screen flex justify-center items-center bg-[#000000a9]'>
-        <div className='w-[70%] h-[80%] bg-gray-800 rounded-lg py-7 px-20 relative'>
-          <h2 className='text-center text-5xl'>About Me</h2>
-          <p className=''>
-            Hi! I&lsquo;m Mikyle and I&lsquo;m a Fullstack Developer and
-            investor from the Philippines, currently living and working in
-            Vancouver. My coding journey started early in university where I
-            discovered coding through my older sister! In 2021, I realized that
-            coding was exactly what I wanted to do in life so I took the leap
-            and decided to quit my job and learn coding full-time.
-          </p>
-          <p className=''>
-            Since then, I have been on an exciting journey of learning and
-            creating, fueled by my passion for design aesthetics and creative
-            learning. What started out as a hobby turned quickly into a passion
-            of work and lifelong learning. In coding, I found a perfect mix of
-            designing, creating, and problem-solving.
-          </p>
-          <p className=''>
-            My current tech stack includes JavaScript, React.js, Next.js,
-            TailwindCSS, and Ruby on Rails. I am currently in the process of
-            learning Python and Node.js.
-          </p>
+        <div className='max-w-[80%] max-h-[90%] text-center sm:text-left bg-gray-800 rounded-lg py-7 sm:px-20 overflow-auto'>
+          <div className='flex justify-center flex-wrap xl:flex-nowrap gap-8 h-[85%]'>
+            <div className='self-center w-[60%] xl:max-w-[40%]'>
+              <Image
+                src={PictureOfMe2}
+                alt='Picture of me in Modal'
+                layout='responsive'
+                className='rounded-xl'
+              />
+            </div>
+            <div className='flex flex-col gap-6 xl:gap-0 justify-between max-w-[60%]'>
+              <h2 className='text-center text-4xl sm:text-5xl'>About Me</h2>
+              <p>
+                Hi! I&lsquo;m Mikyle and I&lsquo;m a Fullstack Developer and
+                investor from the Philippines, currently living and working in
+                Vancouver. My coding journey started early in university where I
+                discovered coding through my older sister! In 2021, I realized
+                that coding was exactly what I wanted to do in life. So I took
+                the leap and decided to quit my job to learn coding full-time.
+              </p>
+              <p>
+                Since then, I have been on an exciting journey of learning and
+                creating, fueled by my passion for design aesthetics and
+                creative learning. What started out as a hobby turned quickly
+                into a passion of work and lifelong learning. In coding, I found
+                a perfect mix of designing, creating, and problem-solving.
+              </p>
+              <p>
+                My current tech stack includes JavaScript, React.js, Next.js,
+                TailwindCSS, and Ruby on Rails. I am currently in the process of
+                learning Python and Node.js.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
