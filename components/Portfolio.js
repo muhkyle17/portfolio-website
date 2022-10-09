ddafdf1180a954d2bb8d5ad3b5c5c0116bb4aced
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-10-08 22:22:06 -0700

    Adjust padding of portfolio section to fit the project as 'portfolio' nav is clicked

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -7,7 +7,7 @@ import Projects from './Projects'
 const Portfolio = () => {
   return (
     <section id='portfolio' className='min-h-fit'>
-      <div className='container mx-auto h-full py-12 lg:py-14'>
+      <div className='container mx-auto h-full py-12 lg:py-6'>
         <div className='flex flex-col gap-4 items-center mb-6'>
           <h1 className='text-5xl font-semibold'>PORTFOLIO</h1>
           <p className='text-2xl'>My latest work</p>
