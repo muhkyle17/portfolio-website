a91434138f92e861d6fa47fe1c0a1745263d306d
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-10-15 22:20:10 -0700

    Minor changes

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <section id='footer' className='h-28 bg-secondary hidden sm:block'>
       <div className='container mx-auto flex flex-col h-full items-center justify-center'>
         <p className='flex items-center text-base'>
-          Coded with <FiHeart className='mx-2' /> by Mikyle Reyes using React.js
+          Coded with <FiHeart className='mx-2' /> by Mikyle Reyes using Next.js
           and Tailwind CSS.
         </p>
         {/* <p className='text-base'>Designed by Cristian Mihai.</p> */}
