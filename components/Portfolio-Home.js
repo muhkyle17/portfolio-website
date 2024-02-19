4d4dd064f6fb0820e0e475343ad269de116cc1c7
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-18 23:34:03 -0700

    Lessen the gap between button and projects portion

--- components/Portfolio-Home.js
+++ components/Portfolio-Home.js
@@ -7,7 +7,7 @@ const PortfolioHome = () => {
 
   return (
     <section id='portfolio' className='min-h-fit bg-secondary py-14'>
-      <div className='container mx-auto flex flex-col gap-5 h-full py-12 px-12 lg:py-6'>
+      <div className='container mx-auto flex flex-col gap-2 h-full py-12 px-12 lg:py-6'>
         <div className='flex flex-col gap-4 items-center mb-6'>
           <h1 className='text-5xl font-semibold mb-8'>Recent Projects</h1>
           <Projects />
