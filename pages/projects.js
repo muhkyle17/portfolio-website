d333d2f334bac65bcf8e2ddf98a6331f2bf14dfc
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-19 00:08:25 -0700

    Add the necessary components and styling to the projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,15 @@
+import Header from '../components/common/Header'
+import Footer from '../components/common/Footer'
+import Portfolio from '../components/Portfolio'
+
 const ProjectsPage = () => {
   return (
-    <div>
-      <p>Projects Page</p>
+    <div className='relative'>
+      <Header />
+      <div className='mt-24 mb-10'>
+        <Portfolio />
+      </div>
+      <Footer />
     </div>
   )
 }
