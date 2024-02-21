5543cd6421438ae498c770c473fa6626ddb96bae
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-20 23:34:26 -0700

    Move the experience ui to its own component

--- components/resume/Experience.js
+++ components/resume/Experience.js
@@ -1,7 +1,49 @@
+import Link from 'next/link'
+import { experience } from '../../data'
+
 const Experience = () => {
   return (
-    <div>
-      <p>Experience</p>
+    <div className='flex flex-col gap-8 w-4/6'>
+      <div className='flex w-full justify-between items-center'>
+        <h1 className='text-3xl'>Experience</h1>
+
+        <div className='flex gap-5'>
+          <div className='py-2 px-4 w-fit text-sm sm:text-base font-medium text-white flex items-center justify-center rounded-full hover:bg-primaryDarker bg-primary cursor-pointer hover:bg-secondary-hover md:btn-lg transition-all'>
+            <Link download href='/Mikyle-Resume.pdf' passHref>
+              <a target='_blank' rel='noopener noreferrer'>
+                Download Resume
+              </a>
+            </Link>
+          </div>
+          <a
+            className='py-2 px-4 w-fit text-sm sm:text-base font-medium text-white flex items-center justify-center rounded-full hover:bg-primaryDarker bg-primary hover:bg-secondary-hover md:btn-lg transition-all'
+            target='_blank'
+            rel='noreferrer'
+            href='https://mikylereyes.notion.site/Resume-cb5577d7692e4fa08afc58c0d741694e?pvs=4'>
+            Notion Resume
+          </a>
+        </div>
+      </div>
+
+      {experience.map(job => (
+        <div key={job.company} className='flex gap-5 px-10 py-14 bg-primary rounded-[45px]'>
+          <div className='w-2/5 text-[#EEEEEE]'>
+            <h2 className='font-bold text-2xl'>{job.company}</h2>
+            <p className='font-semibold text-lg'>{job.jobTitle}</p>
+            <p className='font font-medium text-base'>{job?.jobType}</p>
+            <p className='mt-5 text-sm'>{job.duration}</p>
+            <p className='text-sm'>{job.location}</p>
+          </div>
+
+          <div className='w-3/5'>
+            <ul className='list-disc flex flex-col gap-2 text-[#EEEEEE]'>
+              {job.responsibilities.map(work => (
+                <li key={Math.random()}>{work}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
--- components/resume/Resume.js
+++ components/resume/Resume.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { FiGithub } from 'react-icons/fi'
 import { experience, resumeProjects as projects } from '../../data'
+import Experience from './Experience'
 
 const Resume = () => {
   return (
@@ -8,48 +9,7 @@ const Resume = () => {
       <div className='flex flex-col items-center gap-16'>
         <h1 className='text-5xl'>Resume</h1>
 
-        <div className='flex flex-col gap-8 w-4/6'>
-          <div className='flex w-full justify-between items-center'>
-            <h1 className='text-3xl'>Experience</h1>
-
-            <div className='flex gap-5'>
-              <div className='py-2 px-4 w-fit text-sm sm:text-base font-medium text-white flex items-center justify-center rounded-full hover:bg-primaryDarker bg-primary cursor-pointer hover:bg-secondary-hover md:btn-lg transition-all'>
-                <Link download href='/Mikyle-Resume.pdf' passHref>
-                  <a target='_blank' rel='noopener noreferrer'>
-                    Download Resume
-                  </a>
-                </Link>
-              </div>
-              <a
-                className='py-2 px-4 w-fit text-sm sm:text-base font-medium text-white flex items-center justify-center rounded-full hover:bg-primaryDarker bg-primary hover:bg-secondary-hover md:btn-lg transition-all'
-                target='_blank'
-                rel='noreferrer'
-                href='https://mikylereyes.notion.site/Resume-cb5577d7692e4fa08afc58c0d741694e?pvs=4'>
-                Notion Resume
-              </a>
-            </div>
-          </div>
-
-          {experience.map(job => (
-            <div key={job.company} className='flex gap-5 px-10 py-14 bg-primary rounded-[45px]'>
-              <div className='w-2/5 text-[#EEEEEE]'>
-                <h2 className='font-bold text-2xl'>{job.company}</h2>
-                <p className='font-semibold text-lg'>{job.jobTitle}</p>
-                <p className='font font-medium text-base'>{job?.jobType}</p>
-                <p className='mt-5 text-sm'>{job.duration}</p>
-                <p className='text-sm'>{job.location}</p>
-              </div>
-
-              <div className='w-3/5'>
-                <ul className='list-disc flex flex-col gap-2 text-[#EEEEEE]'>
-                  {job.responsibilities.map(work => (
-                    <li key={Math.random()}>{work}</li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          ))}
-        </div>
+        <Experience />
 
         <div className='flex flex-col gap-8 w-4/6'>
           <h1 className='text-3xl'>Projects</h1>
