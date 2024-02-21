e450d310d9ff74d870b59dc4619c451796dea800
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-21 00:28:21 -0700

    Show the tech stacks and skills

--- components/resume/TechSkills.js
+++ components/resume/TechSkills.js
@@ -1,7 +1,24 @@
-const TechSkills = () => {
+const TechSkills = ({ techSkills }) => {
   return (
-    <div>
-      <p>Tech Skills</p>
+    <div className='flex flex-col gap-8 w-4/6'>
+      <h1 className='text-3xl'>Technical Skills</h1>
+
+      <div className='flex gap-5 px-10 py-10 bg-primary rounded-[45px]'>
+        <div className='flex flex-col gap-5 w-[100%]'>
+          {techSkills.map(skill => (
+            <div key={Math.random()}>
+              <h3 className='font-bold'>
+                {skill.type}:{' '}
+                {skill.techStacks.map(stack => (
+                  <span key={Math.random} className='font-normal'>
+                    {`${stack}, `}
+                  </span>
+                ))}
+              </h3>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
