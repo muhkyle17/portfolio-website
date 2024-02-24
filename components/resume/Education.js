010a305ea23bb29458e4b670244d4fea059acd7a
Mikyle Reyes <mikylereyes@Mikyles-MacBook-Air-2.local>
2024-02-24 09:47:43 -0800

    Change the font of the school titles

--- components/resume/Education.js
+++ components/resume/Education.js
@@ -9,9 +9,9 @@ const Education = ({ education }) => {
             key={Math.random()}
             className='flex gap-5 px-10 py-10 bg-transparent border-2 border-primary rounded-[45px]'>
             <div className='flex flex-col gap-3 w=[100%]'>
-              <h1 className='text-xl font-semibold'>
+              <h2 className='text-xl font-semibold'>
                 {edu.program}, <span className='italic'>{edu.school}</span>
-              </h1>
+              </h2>
               <p>{edu.duration}</p>
               <p>{edu.location}</p>
             </div>
