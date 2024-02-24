57e62862bbf35acbe20c9005b5110b4ec3c88117
Mikyle Reyes <mikylereyes@Mikyles-MacBook-Air-2.local>
2024-02-24 10:32:40 -0800

    Change the colors of components

--- components/Portfolio-Home.js
+++ components/Portfolio-Home.js
@@ -6,7 +6,7 @@ const PortfolioHome = () => {
   const router = useRouter()
 
   return (
-    <section className='min-h-fit bg-secondary py-8'>
+    <section className='min-h-fit bg-tertiary py-8'>
       <div className='container mx-auto flex flex-col gap-7 h-full py-12 px-12 lg:py-6'>
         <div className='flex flex-col gap-9 items-center'>
           <h1 className='text-5xl font-semibold'>Recent Projects</h1>
--- components/Skills.js
+++ components/Skills.js
@@ -7,19 +7,13 @@ import { skills } from '../data'
 
 const Skills = () => {
   return (
-    <section className='h-[20vh] bg-[#131419]'>
+    <section className='h-[20vh]'>
       <div className='container mx-auto h-full'>
         <ul className='flex items-center justify-between h-full w-full'>
           {skills.map((item, index) => {
             return (
               <li key={index}>
-                <Image
-                  src={item.image}
-                  alt={item.alt}
-                  width='86'
-                  height='86'
-                  layout='intrinsic'
-                />
+                <Image src={item.image} alt={item.alt} width='86' height='86' layout='intrinsic' />
               </li>
             )
           })}
