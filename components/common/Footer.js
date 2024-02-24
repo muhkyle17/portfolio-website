a33770253d6f60b42a25664b9d072997b0706a54
Mikyle Reyes <mikylereyes@Mikyles-MacBook-Air-2.local>
2024-02-24 10:23:02 -0800

    Change the color of the tertiary

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -3,11 +3,10 @@ import { FiHeart } from 'react-icons/fi'
 
 const Footer = () => {
   return (
-    <section id='footer' className='h-28 bg-secondary hidden sm:block'>
+    <section id='footer' className='h-28 bg-tertiary hidden sm:block'>
       <div className='container mx-auto flex flex-col h-full items-center justify-center'>
         <p className='flex items-center text-base'>
-          Coded with <FiHeart className='mx-2' /> by Mikyle Reyes using Next.js
-          and Tailwind CSS.
+          Coded with <FiHeart className='mx-2' /> by Mikyle Reyes using Next.js and Tailwind CSS.
         </p>
         {/* <p className='text-base'>Designed by Cristian Mihai.</p> */}
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: {
       padding: {
@@ -16,7 +13,9 @@ module.exports = {
         primary: '#FFA112',
         primaryDarker: '#FFA012CB',
         secondary: '#1C1D24',
-        tertiary: '#131419',
+        // tertiary: '#131419',
+        // tertiary: '#1E1E1E',
+        tertiary: '#141414',
         accent: {
           DEFAULT: '#ac6b34',
           hover: '#925a2b',
