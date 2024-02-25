20f4362782aefff54e63412f2c4c77b7b5dc561c
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-24 22:24:11 -0800

    Add styling for the contact form

--- components/ContactMe.js
+++ components/ContactMe.js
@@ -21,7 +21,59 @@ const ContactMe = () => {
 
       <div className='hidden lg:block h-[35rem] border-r-2 border-primary' />
 
-      <div></div>
+      <div className='mb-20 w-full md:mb-0 lg:w-1/2'>
+        <form
+          action='https://formsubmit.co/reyesmikyle17@gmail.com'
+          method='POST'
+          className='flex flex-col gap-7'>
+          <input
+            type='text'
+            name='firstName'
+            placeholder='Your first name'
+            className='w-full py-2 bg-transparent border-b border-primary font-normal text-xl placeholder:text-gray-300 focus:outline-none'
+            required
+          />
+
+          <input
+            type='text'
+            name='lastName'
+            placeholder='Your last name'
+            className='w-full py-2 bg-transparent border-b border-primary font-normal text-xl placeholder:text-gray-300 focus:outline-none'
+            required
+          />
+
+          <input
+            type='email'
+            name='email'
+            placeholder='Email address'
+            className='w-full py-2 bg-transparent border-b border-primary font-normal text-xl placeholder:text-gray-300 focus:outline-none'
+            required
+          />
+          <input
+            type='text'
+            name='company'
+            placeholder='Company Name'
+            className='w-full py-2 bg-transparent border-b border-primary font-normal text-xl placeholder:text-gray-300 focus:outline-none'
+            required
+          />
+
+          <textarea
+            type='text'
+            name='message'
+            rows='4'
+            cols='50'
+            className='w-full py-2 bg-transparent border-b border-primary font-normal text-xl placeholder:text-gray-300 focus:outline-none'
+            placeholder='Leave a message...'
+          />
+
+          <button
+            type='submit'
+            className='bg-primary border-2 border-primary rounded-full py-3 px-6 w-fit font-medium text-white hover:bg-transparent hover:text-primary transition-all duration-300'>
+            Send Message
+          </button>
+          <input type='hidden' name='_captcha' value='false' />
+        </form>
+      </div>
     </section>
   )
 }
