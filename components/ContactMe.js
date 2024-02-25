9fcf5d861032aaf3431b8ad7ac26a4e624ab8e95
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-24 22:18:23 -0800

    Add styling to contact me page

--- components/ContactMe.js
+++ components/ContactMe.js
@@ -1,8 +1,28 @@
+import { social } from '../data'
+import Social from '../components/Social'
+
 const ContactMe = () => {
   return (
-    <div>
-      <p>Contact me Component</p>
-    </div>
+    <section className='container mx-auto flex flex-wrap lg:flex-nowrap items-center justify-between gap-10 lg:gap-20 w-full h-full pt-32// mt-[5rem] overflow-hidden relative py-12 lg:py-[3.6rem]'>
+      <div className='flex flex-col items-start justify-center lg:w-1/2 gap-8 lg:gap-10'>
+        <h2 className='text-6xl sm:text-7xl tracking-wide font-semibold text-primary'>
+          Reach out.
+        </h2>
+
+        <div className='flex flex-col gap-1'>
+          <h2 className='text-3xl font-semibold'>I&lsquo;d love to help.</h2>
+          <h2 className='text-lg sm:text-xl tracking-wide'>
+            Let me know what you need. I&lsquo;ll get in touch within 24 hours
+          </h2>
+        </div>
+
+        <Social iconSize='text-2xl' />
+      </div>
+
+      <div className='hidden lg:block h-[35rem] border-r-2 border-primary' />
+
+      <div></div>
+    </section>
   )
 }
 
--- pages/contact-me.js
+++ pages/contact-me.js
@@ -1,7 +1,25 @@
+import Head from 'next/head'
+
+import Header from '../components/common/Header'
+import Footer from '../components/common/Footer'
+import ContactMeComponent from '../components/ContactMe'
+
 const ContactMe = () => {
   return (
-    <div>
-      <p>Contact Me Page</p>
+    <div className='relative h-screen'>
+      <Head>
+        <title>Mikyle Reyes</title>
+        <meta name='description' content='Mikyle Reyes' />
+        <link rel='icon' href='/notion-avatar-1663964359003.svg' />
+      </Head>
+
+      <Header />
+      <div>
+        <ContactMeComponent />
+      </div>
+      <div className='absolute bottom-0 w-full'>
+        <Footer />
+      </div>
     </div>
   )
 }
