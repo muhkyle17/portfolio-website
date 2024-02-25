f023ef0c4fc4cb8d1ab5ac44fce44cdae6a14929
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-24 22:58:51 -0800

    Add more spacing to the middle nav

--- components/common/Header.js
+++ components/common/Header.js
@@ -26,14 +26,14 @@ const Header = ({ hidden }) => {
         <button
           onClick={() => router.push('/')}
           type='button'
-          className='name text-2xl hover:text-primary transition-all duration-300 w-1/3 flex justify-start'>
+          className='name text-2xl hover:text-primary transition-all duration-300 w-1/4 flex justify-start'>
           MR
         </button>
 
-        <div className='hidden lg:block w-1/3'>
+        <div className='hidden lg:block w-2/4'>
           <Nav />
         </div>
-        <div className='hidden lg:block w-1/3'>
+        <div className='hidden lg:block w-1/4'>
           <Social />
         </div>
         <div className='lg:hidden'>
