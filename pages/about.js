ef7cc57dd95098272e84ec40cc04c8e4e37cbae2
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-04 17:30:25 +0800

    Import skills component with structure

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Header from '../components/common/Header'
 import Footer from '../components/common/Footer'
-
 import About from '../components/About'
+import Skills from '../components/about/Skills'
 
 const AboutMePage = () => {
   return (
@@ -13,8 +13,11 @@ const AboutMePage = () => {
         <link rel='icon' href='/notion-avatar-1663964359003.svg' />
       </Head>
       <Header />
-      <div className='mt-24 mb-[7.2rem]'>
+      <div className='mt-24'>
         <About />
+      </div>
+      <div className='mt-24 mb-[7.2rem] bg-tertiary'>
+        <Skills />
       </div>
       <Footer />
     </div>
