33256c0233fba2b2923ecf369e8523b183da1f10
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-17 23:17:20 -0700

    Add a loading gif for when the project info is still being fetched

--- components/projects/Project.js
+++ components/projects/Project.js
@@ -2,17 +2,40 @@ import { useRouter } from 'next/router'
 import Image from 'next/image'
 
 import { projects } from '../../data'
+import spinner from '../../public/spinner.gif'
 
 const Project = () => {
   const router = useRouter()
 
   const projectInfo = projects.find(project => project.projectRoute === router.query.id)
-  const { title, techType, appType, image, description, skills, live, source } = projectInfo
+  const { title, techType, appType, image, description, skills, live, source } = projectInfo || {}
+
+  if (projectInfo === undefined)
+    return (
+      <div className='flex items-center justify-center h-full'>
+        <Image src={spinner} alt='Spinner' width={500} height={500} />
+      </div>
+    )
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <Image src={image} alt='Project Image' />
+    <div className='container mx-auto px-auto px-48'>
+      <h1 className='text-4xl'>{title}</h1>
+
+      <Image
+        src={image}
+        alt={title}
+        width='650'
+        height='380'
+        layout='intrinsic'
+        className='rounded-t-2xl'
+        priority
+      />
+      <p>{description}</p>
+      <div>{skills}</div>
+      <div>
+        <a>{live}</a>
+        <a>{source}</a>
+      </div>
     </div>
   )
 }
