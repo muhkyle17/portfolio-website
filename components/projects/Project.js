825df789c41e2234274a368cc7dd480c1cfba33c
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-24 13:46:50 -0700

    Add styling to the rest of the page

--- components/projects/Project.js
+++ components/projects/Project.js
@@ -32,23 +32,45 @@ const Project = () => {
     <div className='container mx-auto px-auto xl:px-72 flex flex-col gap-5 items-start'>
       <h1 className='text-5xl text-primary'>[ {title} ]</h1>
 
-      <div>
-        <p>
+      <div className='flex flex-col'>
+        <h1 className='text-2xl'>
           {techType}, {appType}
-        </p>
-        <p>
-          {projectType} | {duration}
-        </p>
+        </h1>
+        <h1 className='text-lg flex flex-row items-center gap-2'>
+          {projectType} <span className='text-3xl'>·</span> {duration}
+        </h1>
+        <div className='mt-3 flex gap-3'>
+          {skills.map((item, index) => {
+            return (
+              <p
+                key={index}
+                className='bg-primary text-center flex items-center justify-center rounded-full py-[0.4rem] px-4 text-xs sm:text-sm'>
+                {item}
+              </p>
+            )
+          })}
+        </div>
       </div>
 
       <div className='self-start relative w-full h-[30vh] sm:h-[50vh] 2xl:h-[35rem]'>
         <Image src={image} alt={title} layout='fill' objectFit='contain' />
       </div>
       <p>{description}</p>
-      <div>{skills}</div>
-      <div>
-        <a>{live}</a>
-        <a>{source}</a>
+      <div className='flex gap-5'>
+        <a
+          href={live}
+          target='_blank'
+          rel='noreferrer'
+          className='bg-transparent border-2 border-primary hover:bg-primary hover:border-primary transition-all duration-600 duration-300 rounded-full py-[0.4rem] px-7 text-xs sm:text-sm'>
+          Live
+        </a>
+        <a
+          href={source}
+          target='_blank'
+          rel='noreferrer'
+          className='bg-transparent border-2 border-primary hover:bg-primary hover:border-primary transition-all duration-600 duration-300 rounded-full py-[0.4rem] px-7 text-xs sm:text-sm'>
+          Source
+        </a>
       </div>
     </div>
   )
