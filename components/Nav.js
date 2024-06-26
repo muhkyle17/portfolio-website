cf5a2502eb23a54c8d60e9755e114680198d3e15
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-25 19:26:39 -0700

    Make projects route dynamic

--- components/Nav.js
+++ components/Nav.js
@@ -5,6 +5,13 @@ import { navigation } from '../data'
 
 const Nav = () => {
   const router = useRouter()
+
+  const dynamicRouter = item => {
+    if (router.asPath === item.href) return 'text-primary border-b-2 border-b-primary'
+    if (router.asPath.includes('/projects') && item.href === '/projects')
+      return 'text-primary border-b-2 border-b-primary'
+  }
+
   return (
     <nav>
       <ul className='flex gap-14 text-sm capitalize items-center justify-center'>
@@ -13,7 +20,11 @@ const Nav = () => {
             <li className='text-white' key={index}>
               <button
                 onClick={() => router.push(`/${item.href}`)}
-                className='border-b border-transparent hover:border-primary hover:text-primary cursor-pointer transition-all duration-300 py-[3px]'>
+                className={`
+                  border-b-2 border-transparent cursor-pointer transition-all duration-300 py-[3px]
+                  hover:border-primary hover:text-primary
+                  ${dynamicRouter(item)}
+                  `}>
                 {item.name}
               </button>
             </li>
--- data.js
+++ data.js
@@ -64,7 +64,7 @@ export const navigation = [
   // },
   {
     name: 'Contact',
-    href: 'contact-me',
+    href: '/contact-me',
   },
 ]
 
