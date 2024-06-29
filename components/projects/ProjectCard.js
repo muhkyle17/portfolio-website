f531865dd1a05750c7ba3b7c3ecd8147d6e81eb7
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-29 09:15:56 -0700

    Make the font size dynamic using title.length

--- components/projects/ProjectCard.js
+++ components/projects/ProjectCard.js
@@ -16,6 +16,9 @@ const ProjectCard = ({
 }) => {
   const router = useRouter()
 
+  console.log(projectRoute, 'projectRoute')
+  console.log(title.length, 'title.length')
+
   return (
     <motion.div
       className='box flex flex-col mb-2 border-2 border-primary rounded-t-xl// rounded-2xl cursor-pointer hover:ease-in transition-all duration-500'
@@ -36,12 +39,7 @@ const ProjectCard = ({
       />
       <div className='flex flex-col py-5 px-7 bg-transparent'>
         {/* //TODO: Find a more sustainable way of making the font size here smaller */}
-        <p
-          className={`${
-            projectRoute === 'ngs' || projectRoute === 'creator-affiliate' ? 'text-2xl' : 'text-3xl'
-          } text-primary `}>
-          {title}
-        </p>
+        <p className={`${title.length > 20 ? 'text-2xl' : 'text-3xl'} text-primary `}>{title}</p>
         <p className='mt-4 text-base font-semibold'>{techType}</p>
         <p className='text-base font-semibold'>{appType}</p>
       </div>
