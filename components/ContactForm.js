805d74f150c3b1139d755568070af7dbeaf6b015
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-12 18:44:46 -0700

    Add modal logic for when a contact form is submitted

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,6 +1,50 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { FiX } from 'react-icons/fi'
+import { FaRegCheckCircle } from 'react-icons/fa'
 
 import Social from './Social'
+
+function Modal({ setShowModal }) {
+  const closeOnEscapeKeyDown = e => {
+    if ((e.charCode || e.keyCode) === 27) {
+      setShowModal(false)
+    }
+  }
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', closeOnEscapeKeyDown)
+    return function cleanup() {
+      document.body.removeEventListener('keydown', closeOnEscapeKeyDown)
+    }
+  })
+
+  return (
+    <div className='h-screen fixed bottom-0 left-0 z-50'>
+      <div
+        className='h-screen w-screen flex justify-center items-center bg-[#00000081]'
+        onClick={() => setShowModal(false)}>
+        <div
+          className='w-[60%] max-h-[40rem] text-center xl:text-left bg-[#232323] rounded-2xl py-16 sm:px-12 overflow-auto relative'
+          onClick={e => e.stopPropagation()}>
+          <FiX
+            className='absolute top-4 right-5 text-2xl cursor-pointer hover:text-primary transition-all duration-500'
+            onClick={() => setShowModal(false)}
+          />
+
+          <div className='max-w-[90%] mx-auto flex justify-center flex-wrap xl:flex-nowrap h-[30rem] '>
+            <div className='flex flex-col gap-3 items-center xl:gap-6 self-center text-base'>
+              <FaRegCheckCircle className='text-primary text-9xl' />
+              <h2 className='text-primaryDarker font-semibold text-4xl sm:text-5xl'>Thank you!</h2>
+              <p className='text-xl text-center'>
+                We&lsquo;ve received your message. Someone from our team will contact you soon.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -10,7 +54,6 @@ const ContactForm = () => {
     companyName: '',
     message: '',
   })
-
   const [responseMessage, setResponseMessage] = useState('')
 
   const handleChange = e => {
@@ -117,6 +160,9 @@ const ContactForm = () => {
             className='bg-primary border-2 border-primary rounded-full py-3 px-6 w-fit font-medium text-white hover:bg-transparent hover:text-primary transition-all duration-500'>
             Send Message
           </button>
+
+          {/* <div className='hidden// opacity-0//'>{responseMessage}</div> */}
+          {showModal && <Modal setShowModal={setShowModal} />}
         </form>
       </div>
     </section>
