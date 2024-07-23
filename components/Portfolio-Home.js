d5b366d25201c4341ba6282f68233357657f5672
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-23 00:44:29 -0700

    Make recent projects text center

--- components/Portfolio-Home.js
+++ components/Portfolio-Home.js
@@ -9,7 +9,7 @@ const PortfolioHome = () => {
     <section className='min-h-fit bg-tertiary py-8'>
       <div className='container mx-auto flex flex-col gap-7 h-full py-12 px-12 lg:py-6'>
         <div className='flex flex-col gap-9 items-center'>
-          <h1 className='text-5xl font-semibold'>Recent Projects</h1>
+          <h1 className='text-5xl font-semibold text-center'>Recent Projects</h1>
           <Projects shortened />
         </div>
         <button
