58113578e210a4418c31b2acd94f00d199e0b9fa
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-08-29 14:58:23 -0400

    Return only the project properties from the payload

--- pages/api/notion/projects.js
+++ pages/api/notion/projects.js
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: 'Only GET  requests are allowed.' })
 
   const data = await notion.databases.query({ database_id })
-  const payload = data.results
-  res.status(200).json({ payload })
+  const projects = data.results.map(result => result.properties)
+
+  res.status(200).json({ projects })
 }
