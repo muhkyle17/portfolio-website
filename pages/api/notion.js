bd97f44071aa5d54939f6e082eccc2e279201219
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-08-28 22:28:55 -0400

    Simplify code

--- pages/api/notion.js
+++ pages/api/notion.js
@@ -4,18 +4,7 @@ const notion = new Client({ auth: process.env.NOTION_API_KEY })
 const database_id = process.env.NOTION_DATABASE_ID
 
 export default async function handler(req, res) {
-  let payload = []
-
-  async function getImages() {
-    const data = await notion.databases.query({
-      database_id: database_id,
-    })
-    return data
-  }
-
-  await getImages().then(data => {
-    payload = data.results
-  })
-
+  const data = await notion.databases.query({ database_id })
+  const payload = data.results
   res.status(200).json({ payload })
 }
