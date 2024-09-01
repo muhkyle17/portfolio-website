395b32f732aa6d290170c8b556fd6bb4f4d95946
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-09-01 10:25:48 -0400

    Make the call to notion

--- components/projects/Projects.js
+++ components/projects/Projects.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 // import ProjectOldDisplay from '../Project-Old-Display'
 import ProjectCard from './ProjectCard'
 import { projects, projectsShortened } from '../../data'
 
 const Projects = ({ shortened }) => {
+  const [notionProjects, setNotionProjects] = useState([])
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const { data } = await axios.get('/api/notion/projects')
+        setNotionProjects(data.projects)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchProjects()
+  }, [])
+
+  console.log(notionProjects, 'notionProjects')
+
   return (
     <div className='flex flex-wrap gap-10 justify-around lg:justify-between xl:justify-center items-baseline'>
       {(shortened ? projectsShortened : projects).map((item, index) => {
