4aab412b2d57986af91aa4b4446c1c7a8568ccdc
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-09-01 16:49:17 -0400

    Add loader into project list cards

--- components/projects/Projects.js
+++ components/projects/Projects.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 // import ProjectOldDisplay from '../Project-Old-Display'
 import ProjectCard from './ProjectCard'
 import { projects, projectsShortened } from '../../data'
+import Loader from '../common/Loader'
 
 const Projects = ({ shortened }) => {
   const [notionProjects, setNotionProjects] = useState([])
@@ -21,23 +22,28 @@ const Projects = ({ shortened }) => {
     fetchProjects()
   }, [])
 
-  console.log(notionProjects, 'notionProjects')
-
   const shortenedProjects = notionProjects.slice(0, 3)
+
+  if (notionProjects.length === 0) {
+    return (
+      <div className='flex px-4 h-[70vh] w-full items-center justify-center'>
+        <Loader />
+      </div>
+    )
+  }
 
   return (
     <div className='flex flex-wrap gap-10 justify-around lg:justify-between xl:justify-center items-baseline'>
       {(shortened ? shortenedProjects : notionProjects)?.map((project, index) => {
-        const image = projects.filter(
-          item => item.title === project?.title?.title?.[0]?.plain_text
-        )[0]?.image?.src
-
         return (
           <ProjectCard
             key={index}
             title={project?.title?.title[0]?.plain_text || ''}
             projectRoute={project?.projectRoute?.rich_text[0]?.plain_text || ''}
-            image={image}
+            image={
+              projects.filter(item => item.title === project?.title?.title?.[0]?.plain_text)[0]
+                ?.image?.src
+            }
             techType={project?.techType?.rich_text[0]?.plain_text || ''}
             appType={project?.appType?.rich_text[0]?.plain_text || ''}
             description={project?.description?.rich_text[0]?.plain_text || ''}
